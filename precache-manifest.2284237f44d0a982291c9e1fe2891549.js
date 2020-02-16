3123bd9828b34c450e3e13b4598e6791888548ac
Betty Chow <bettychchow@gmail.com>
2020-02-16 11:06:01 -0800

    Updates

---
+++ precache-manifest.2284237f44d0a982291c9e1fe2891549.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb93446c50f1547f1d0eaf487fb78538",
+    "url": "/kitties-rolodex/index.html"
+  },
+  {
+    "revision": "613c36fd533eb19de3ee",
+    "url": "/kitties-rolodex/static/css/main.c180944f.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/kitties-rolodex/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/kitties-rolodex/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "613c36fd533eb19de3ee",
+    "url": "/kitties-rolodex/static/js/main.c959a216.chunk.js"
+  },
+  {
+    "revision": "c03c94a57f8284ca625b",
+    "url": "/kitties-rolodex/static/js/runtime-main.1bb80f1f.js"
+  }
+]);
